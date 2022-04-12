8244763b4b26ae21aa445a4e2aaa35e5354c1c8d
Muhammad Ahmar Hilal <62645337+ahmarcode@users.noreply.github.com>
2022-04-13 04:56:13 +0530

    Add hello world page

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+
+app.use(express.static("./public"));
+
+app.get('/', (req, res) => {
+    res.sendFile('./index.html');
+})
+
+const port = 3000;
+
+app.listen(
+    port, () => {
+        console.log(`Server running on port ${port}`);
+    }
+)
